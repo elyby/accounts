dd423a0c6b8500505ce175cf150c40780d13253b
SleepWalker <mybox@udf.su>
2016-08-05 08:41:33 +0300

    #14: integrate captcha with backend. Add validation message

--- environments/dev/api/config/main-local.php
+++ environments/dev/api/config/main-local.php
@@ -5,6 +5,7 @@ $config = [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
             'cookieValidationKey' => '',
+            'enableCsrfValidation' => false,
         ],
         'reCaptcha' => [
             'public' => '',
