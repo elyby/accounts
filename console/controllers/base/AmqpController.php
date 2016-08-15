c3d0490d4a043a7f1fe7ee896533b6e78b413837
ErickSkrauch <erickskrauch@yandex.ru>
2016-08-16 01:27:18 +0300

    Реорганизован компонент для контроллера amqp очередей в наш внешний пакет

--- console/controllers/base/AmqpController.php
+++ console/controllers/base/AmqpController.php
@@ -1,8 +1,35 @@
 <?php
 namespace console\controllers\base;
 
-use common\components\RabbitMQ\Controller;
+use Ely\Amqp\ControllerTrait;
+use Yii;
+use yii\console\Controller;
+use yii\helpers\ArrayHelper;
+use yii\helpers\Inflector;
 
 abstract class AmqpController extends Controller {
+    use ControllerTrait;
+
+    public final function actionIndex() {
+        $this->start();
+    }
+
+    public function getRoutesMap() {
+        return [];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    protected function getConnection() {
+        return Yii::$app->amqp->getConnection();
+    }
+
+    /**
+     * @inheritdoc
+     */
+    protected function buildRouteActionName($route) {
+        return ArrayHelper::getValue($this->getRoutesMap(), $route, 'route' . Inflector::camelize($route));
+    }
 
 }
