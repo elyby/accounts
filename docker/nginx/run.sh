2ef6ac3c086a96222d2e7b8bf91e4fbb7f45acc4
ErickSkrauch <erickskrauch@yandex.ru>
2016-09-01 20:04:52 +0300

    В nginx теперь можно подсунуть кастомную ссылку на php

--- docker/nginx/run.sh
+++ docker/nginx/run.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-envsubst '$AUTHSERVER_HOST' < /etc/nginx/conf.d/account.ely.by.conf.template > /etc/nginx/conf.d/default.conf
+envsubst '$AUTHSERVER_HOST:$PHP_LINK' < /etc/nginx/conf.d/account.ely.by.conf.template > /etc/nginx/conf.d/default.conf
 nginx -g 'daemon off;'
