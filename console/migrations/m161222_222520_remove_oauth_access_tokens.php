c029db82a1d8dc4fca5e7efc878b60a3543cf7f8
ErickSkrauch <erickskrauch@yandex.ru>
2016-12-23 01:32:44 +0300

    #272: удалены все следы использование таблицы oauth_access_tokens

---
+++ console/migrations/m161222_222520_remove_oauth_access_tokens.php
@@ -0,0 +1,31 @@
+<?php
+
+use console\db\Migration;
+
+class m161222_222520_remove_oauth_access_tokens extends Migration {
+
+    public function safeUp() {
+        $this->dropForeignKey('FK_oauth_access_toke_to_oauth_session', '{{%oauth_access_tokens}}');
+        $this->dropTable('{{%oauth_access_tokens}}');
+    }
+
+    public function safeDown() {
+        $this->createTable('{{%oauth_access_tokens}}', [
+            'access_token' => $this->string(64),
+            'session_id'   => $this->getDb()->getTableSchema('{{%oauth_sessions}}')->getColumn('id')->dbType,
+            'expire_time'  => $this->integer()->notNull(),
+            $this->primary('access_token'),
+        ], $this->tableOptions);
+
+        $this->addForeignKey(
+            'FK_oauth_access_toke_to_oauth_session',
+            '{{%oauth_access_tokens}}',
+            'session_id',
+            '{{%oauth_sessions}}',
+            'id',
+            'CASCADE',
+            'SET NULL'
+        );
+    }
+
+}
--- tests/codeception/console/unit/controllers/CleanupControllerTest.php
+++ tests/codeception/console/unit/controllers/CleanupControllerTest.php
@@ -1,31 +1,8 @@
 <?php
 namespace codeception\console\unit\controllers;
 
-use common\models\OauthAccessToken;
-use console\controllers\CleanupController;
-use tests\codeception\common\fixtures\OauthAccessTokenFixture;
 use tests\codeception\console\unit\TestCase;
-use Yii;
 
 class CleanupControllerTest extends TestCase {
-
-    public function _fixtures() {
-        return [
-            'accessTokens' => OauthAccessTokenFixture::class,
-        ];
-    }
-
-    public function testActionAccessTokens() {
-        /** @var OauthAccessToken $validAccessToken */
-        $validAccessToken = $this->tester->grabFixture('accessTokens', 'admin-test1');
-        /** @var OauthAccessToken $expiredAccessToken */
-        $expiredAccessToken = $this->tester->grabFixture('accessTokens', 'admin-test1-expired');
-
-        $controller = new CleanupController('cleanup', Yii::$app);
-        $this->assertEquals(0, $controller->actionAccessTokens());
-
-        $this->tester->canSeeRecord(OauthAccessToken::class, ['access_token' => $validAccessToken->access_token]);
-        $this->tester->cantSeeRecord(OauthAccessToken::class, ['access_token' => $expiredAccessToken->access_token]);
-    }
 
 }
