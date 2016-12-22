07735a0eed4434388fdb1cde3b14baccfbcba52a
ErickSkrauch <erickskrauch@yandex.ru>
2016-12-23 00:21:05 +0300

    #274: переработан валидатор EmailActivationKeyValidator

--- api/validators/EmailActivationKeyValidator.php
+++ api/validators/EmailActivationKeyValidator.php
@@ -5,30 +5,52 @@ use common\helpers\Error as E;
 use common\models\EmailActivation;
 use yii\validators\Validator;
 
+/**
+ * Валидатор для проверки полученного от пользователя кода активации.
+ * В случае успешной валидации подменяет значение поля на актуальную модель
+ */
 class EmailActivationKeyValidator extends Validator {
+
+    /**
+     * @var int тип ключа. Если не указан, то валидирует по всем ключам.
+     */
+    public $type;
+
+    public $keyRequired = E::KEY_REQUIRED;
 
     public $notExist = E::KEY_NOT_EXISTS;
 
     public $expired = E::KEY_EXPIRE;
 
-    public function validateValue($value) {
-        if (($model = $this->findEmailActivationModel($value)) === null) {
-            return [$this->notExist, []];
+    public function validateAttribute($model, $attribute) {
+        $value = $model->$attribute;
+        if (empty($value)) {
+            $this->addError($model, $attribute, $this->keyRequired);
+            return;
         }
 
-        if ($model->isExpired()) {
-            return [$this->expired, []];
+        $activation = $this->findEmailActivationModel($value, $this->type);
+        if ($activation === null) {
+            $this->addError($model, $attribute, $this->notExist);
+            return;
         }
 
-        return null;
+        if ($activation->isExpired()) {
+            $this->addError($model, $attribute, $this->expired);
+            return;
+        }
+
+        $model->$attribute = $activation;
     }
 
-    /**
-     * @param string $key
-     * @return null|EmailActivation
-     */
-    protected function findEmailActivationModel($key) {
-        return EmailActivation::findOne($key);
+    protected function findEmailActivationModel(string $key, int $type = null): ?EmailActivation {
+        $query = EmailActivation::find();
+        $query->andWhere(['key' => $key]);
+        if ($type !== null) {
+            $query->andWhere(['type' => $type]);
+        }
+
+        return $query->one();
     }
 
 }
