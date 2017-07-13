411ffe0606812b49b8a8c10fca540c41c3b3627e
ErickSkrauch <erickskrauch@yandex.ru>
2017-07-13 13:44:06 +0300

    Переводим пробелы (по стандарту OAuth2) в наш scopes delimiter.

--- api/components/OAuth2/Grants/RefreshTokenGrant.php
+++ api/components/OAuth2/Grants/RefreshTokenGrant.php
@@ -4,6 +4,7 @@ namespace api\components\OAuth2\Grants;
 use api\components\OAuth2\Entities;
 use ErrorException;
 use League\OAuth2\Server\Entity\ClientEntity as OriginalClientEntity;
+use League\OAuth2\Server\Entity\ClientEntity;
 use League\OAuth2\Server\Entity\RefreshTokenEntity as OriginalRefreshTokenEntity;
 use League\OAuth2\Server\Event;
 use League\OAuth2\Server\Exception;
@@ -23,6 +24,21 @@ class RefreshTokenGrant extends \League\OAuth2\Server\Grant\RefreshTokenGrant {
 
     protected function createSessionEntity() {
         return new Entities\SessionEntity($this->server);
+    }
+
+    /**
+     * По стандарту OAuth2 scopes должны разделяться пробелом, а не запятой. Косяк.
+     * Так что оборачиваем функцию разбора скоупов, заменяя пробелы на запятые.
+     *
+     * @param string       $scopeParam
+     * @param ClientEntity $client
+     * @param string $redirectUri
+     *
+     * @return \League\OAuth2\Server\Entity\ScopeEntity[]
+     */
+    public function validateScopes($scopeParam = '', ClientEntity $client, $redirectUri = null) {
+        $scopes = str_replace(' ', $this->server->getScopeDelimiter(), $scopeParam);
+        return parent::validateScopes($scopes, $client, $redirectUri);
     }
 
     /**
