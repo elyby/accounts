ee68e373f4659eab04e7454fb8f583f82a76c4b1
ErickSkrauch <erickskrauch@yandex.ru>
2017-12-02 22:15:00 +0300

    Пересмотрены висящие TODO

--- tests/codeception/config/functional.php
+++ tests/codeception/config/functional.php
@@ -3,7 +3,6 @@ return [
     'components' => [
         'request' => [
             // it's not recommended to run functional tests with CSRF validation enabled
-            // TODO: у нас вроде и без того нет проверки csrf
             'enableCsrfValidation' => false,
             'enableCookieValidation' => false,
             // but if you absolutely need it set cookie domain to localhost
