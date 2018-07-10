92ee2830393314c8fa97c4593bef323ed81ffea7
ErickSkrauch <erickskrauch@yandex.ru>
2018-07-11 02:24:05 +0300

    Upgrade tests environment

--- tests/run-tests.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-cd "$(dirname "$0")"
-
-docker-compose run --rm testphp ./tests/php.sh $*
-docker-compose stop # docker не останавливает зависимые контейнеры после завершения работы главного процесса
