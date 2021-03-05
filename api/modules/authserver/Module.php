077db4f3282ace6a96a9e03c415fee46c557a2b8
ErickSkrauch <erickskrauch@yandex.ru>
2021-03-05 16:07:55 +0100

    Remove authserver module restriction [deploy]

--- api/modules/authserver/Module.php
+++ api/modules/authserver/Module.php
@@ -5,24 +5,14 @@ namespace api\modules\authserver;
 
 use Yii;
 use yii\base\BootstrapInterface;
-use yii\web\NotFoundHttpException;
+use yii\base\Module as BaseModule;
 use yii\web\Response;
 
-class Module extends \yii\base\Module implements BootstrapInterface {
+class Module extends BaseModule implements BootstrapInterface {
 
     public $id = 'authserver';
 
     public $defaultRoute = 'index';
-
-    public function beforeAction($action): bool {
-        if (!parent::beforeAction($action)) {
-            return false;
-        }
-
-        $this->checkHost();
-
-        return true;
-    }
 
     public function afterAction($action, $result) {
         if ($result === null) {
@@ -48,18 +38,6 @@ class Module extends \yii\base\Module implements BootstrapInterface {
 
     public static function error($message) {
         Yii::info($message, 'legacy-authserver');
-    }
-
-    /**
-     * Since this is a legacy method and there will be no documentation for it in the new environment,
-     * there is no point in displaying the internal API, so we are limiting access only to logons from the old domain.
-     *
-     * @throws NotFoundHttpException
-     */
-    protected function checkHost() {
-        if (parse_url(Yii::$app->request->getHostInfo(), PHP_URL_HOST) !== Yii::$app->params['authserverHost']) {
-            throw new NotFoundHttpException();
-        }
     }
 
 }
