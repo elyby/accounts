b98649070d6d975c982a91c8dcf2af01fdf74c80
ErickSkrauch <erickskrauch@yandex.ru>
2023-10-15 04:09:09 +0200

    Enable better logging for queue

--- console/components/ErrorHandler.php
+++ console/components/ErrorHandler.php
@@ -3,20 +3,13 @@ declare(strict_types=1);
 
 namespace console\components;
 
-use Swift_TransportException;
 use Yii;
 use yii\queue\ExecEvent;
 
-class ErrorHandler {
+final class ErrorHandler {
 
-    public function handleQueueError(ExecEvent $error): void {
-        $exception = $error->error;
-        if ($exception instanceof Swift_TransportException) {
-            Yii::warning($exception);
-            return;
-        }
-
-        Yii::error($exception);
+    public function handleQueueError(ExecEvent $event): void {
+        Yii::$app->errorHandler->handleException($event->error);
     }
 
 }
