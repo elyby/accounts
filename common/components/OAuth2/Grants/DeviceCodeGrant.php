45eaf22a9f2d065a3b253392b800b82fd57b42d3
Octol1ttle <l1ttleofficial@outlook.com>
2024-12-25 20:20:12 +0500

    fix: send refresh tokens to Device Code clients too
    Signed-off-by: Octol1ttle <l1ttleofficial@outlook.com>

--- common/components/OAuth2/Grants/DeviceCodeGrant.php
+++ common/components/OAuth2/Grants/DeviceCodeGrant.php
@@ -3,9 +3,13 @@ declare(strict_types=1);
 
 namespace common\components\OAuth2\Grants;
 
+use common\components\OAuth2\Events\RequestedRefreshToken;
 use common\components\OAuth2\Repositories\ExtendedDeviceCodeRepositoryInterface;
+use common\components\OAuth2\Repositories\PublicScopeRepository;
 use common\components\OAuth2\ResponseTypes\EmptyResponse;
 use DateInterval;
+use League\OAuth2\Server\Entities\AccessTokenEntityInterface;
+use League\OAuth2\Server\Entities\ClientEntityInterface;
 use League\OAuth2\Server\Exception\OAuthServerException;
 use League\OAuth2\Server\Grant\DeviceCodeGrant as BaseDeviceCodeGrant;
 use League\OAuth2\Server\Repositories\RefreshTokenRepositoryInterface;
@@ -33,6 +37,22 @@ final class DeviceCodeGrant extends BaseDeviceCodeGrant {
             $verificationUri,
             $retryInterval,
         );
+    }
+
+    protected function issueAccessToken(
+        DateInterval $accessTokenTTL,
+        ClientEntityInterface $client,
+        ?string $userIdentifier,
+        array $scopes = [],
+    ): AccessTokenEntityInterface {
+        foreach ($scopes as $i => $scope) {
+            if ($scope->getIdentifier() === PublicScopeRepository::OFFLINE_ACCESS) {
+                unset($scopes[$i]);
+                $this->getEmitter()->emit(new RequestedRefreshToken('refresh_token_requested'));
+            }
+        }
+
+        return parent::issueAccessToken($accessTokenTTL, $client, $userIdentifier, $scopes);
     }
 
     public function canRespondToAuthorizationRequest(ServerRequestInterface $request): bool {
